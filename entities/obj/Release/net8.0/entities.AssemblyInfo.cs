c1498f74396fb9fdc46c48023f591aaedcaf3deb
MetaMsa <mserhataslan@hotmail.com>
2025-05-11 23:10:54 +0300

    sign

--- entities/obj/Release/net8.0/entities.AssemblyInfo.cs
+++ entities/obj/Release/net8.0/entities.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("entities")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07b72dce7ca84799809ad447c3ecaff7db7690e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("entities")]
