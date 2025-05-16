7af1d68c3466b43cd9dfe94b22694bb8e507b187
MetaMsa <mserhataslan@hotmail.com>
2025-05-16 04:47:43 +0300

    cookies

--- entities/models/UsersModel.cs
+++ entities/models/UsersModel.cs
@@ -6,20 +6,20 @@ namespace Entities.Models
     public class UsersModel
     {
         [Key]
-        public int ID {get; set;}
+        public int ID { get; set; }
 
         [Required(ErrorMessage = "Lütfen Adınızı ve Soyadınızı girin")]
-        public string fullname {get; set;}
+        public string? fullname { get; set; }
 
         [Required(ErrorMessage = "Lütfen kullanıcı adınızı girin")]
-        public string username {get; set;}
+        public string? username { get; set; }
 
         [Required(ErrorMessage = "Lütfen E-Posta adresinizi girin")]
         [EmailAddress(ErrorMessage = "Geçersiz E-Posta adresi")]
-        public string Email {get; set;}
+        public string? Email { get; set; }
 
         [Required(ErrorMessage = "Lütfen şifrenizi girin")]
-        [StringLength(8, ErrorMessage = "Şifre 8 karakterden uzun, 6 karakterden kısa olamaz",MinimumLength=6)]
-        public string password {get; set;}
+        [StringLength(8, ErrorMessage = "Şifre 8 karakterden uzun, 6 karakterden kısa olamaz", MinimumLength = 6)]
+        public string? password { get; set; }
     }
 }
